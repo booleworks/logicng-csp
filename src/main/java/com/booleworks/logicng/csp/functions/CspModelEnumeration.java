0e78c080841899cbd08a49f850d2e061ccb1b4b8
Matthias Jugan <matthias.jugan@booleworks.com>
2024-08-21 12:34:41 +0200

    Add model enumeration for CSPs

---
+++ src/main/java/com/booleworks/logicng/csp/functions/CspModelEnumeration.java
@@ -0,0 +1,73 @@
+package com.booleworks.logicng.csp.functions;
+
+import com.booleworks.logicng.csp.Csp;
+import com.booleworks.logicng.csp.CspAssignment;
+import com.booleworks.logicng.csp.CspFactory;
+import com.booleworks.logicng.csp.encodings.CspEncodingContext;
+import com.booleworks.logicng.csp.encodings.OrderDecoding;
+import com.booleworks.logicng.csp.terms.IntegerVariable;
+import com.booleworks.logicng.formulas.Variable;
+import com.booleworks.logicng.solvers.SATSolver;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CspModelEnumeration {
+    public static List<CspAssignment> enumerate(final SATSolver solver, final Csp csp, final CspEncodingContext context, final CspFactory cf) {
+        return enumerate(solver, csp.getVisibleIntegerVariables(), csp.getVisibleBooleanVariables(), context, cf);
+    }
+
+    public static List<CspAssignment> enumerate(final SATSolver solver, final Collection<IntegerVariable> integerVariables, final Collection<Variable> booleanVariables, final CspEncodingContext context, final CspFactory cf) {
+        final Set<Variable> allVars = context.getSatVariables(integerVariables);
+        allVars.addAll(booleanVariables);
+        final List<IntegerVariable> additionalVariables = integerVariables.stream().filter(v -> !context.getVariableMap().containsKey(v)).collect(Collectors.toList());
+        final List<CspAssignment> decodedModels = solver.enumerateAllModels(allVars).stream()
+                .map(m -> OrderDecoding.decode(m.assignment(), integerVariables, booleanVariables, context, cf))
+                .collect(Collectors.toList());
+        if (additionalVariables.isEmpty() || decodedModels.isEmpty()) {
+            return decodedModels;
+        } else {
+            return enumerateAdditionalVariables(decodedModels, additionalVariables);
+        }
+    }
+
+    private static List<CspAssignment> enumerateAdditionalVariables(final List<CspAssignment> decodedModels, final List<IntegerVariable> integerVariables) {
+        final List<Iterator<Integer>> iterators = integerVariables.stream().map(v -> v.getDomain().iterator()).collect(Collectors.toList());
+        final List<Integer> values = iterators.stream().map(Iterator::next).collect(Collectors.toList()); //Domains cannot be empty
+        final List<CspAssignment> models = new ArrayList<>();
+        int leaderIndex = 0;
+        for (int index = 0; index < integerVariables.size(); ) {
+            for (final CspAssignment model : decodedModels) {
+                final CspAssignment newModel = new CspAssignment(model);
+                for (int i = 0; i < integerVariables.size(); ++i) {
+                    newModel.addIntAssignment(integerVariables.get(i), values.get(i));
+                }
+                models.add(newModel);
+            }
+            if (iterators.get(index).hasNext()) {
+                values.set(index, iterators.get(index).next());
+            } else {
+                while (index < integerVariables.size() && !iterators.get(index).hasNext()) {
+                    ++index;
+                }
+                if (index == integerVariables.size()) {
+                    break;
+                }
+                values.set(index, iterators.get(index).next());
+                for (int i = 0; i < index; ++i) {
+                    iterators.set(i, integerVariables.get(i).getDomain().iterator());
+                    values.set(i, iterators.get(i).next());
+                }
+                if (index > leaderIndex) {
+                    leaderIndex = index;
+                }
+                index = 0;
+            }
+        }
+        return models;
+    }
+}
---
+++ src/test/java/com/booleworks/logicng/csp/functions/CspModelEnumerationTest.java
@@ -0,0 +1,111 @@
+package com.booleworks.logicng.csp.functions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.booleworks.logicng.csp.Csp;
+import com.booleworks.logicng.csp.CspAssignment;
+import com.booleworks.logicng.csp.CspFactory;
+import com.booleworks.logicng.csp.ParameterizedCspTest;
+import com.booleworks.logicng.csp.encodings.CspEncodingContext;
+import com.booleworks.logicng.csp.terms.IntegerVariable;
+import com.booleworks.logicng.formulas.Formula;
+import com.booleworks.logicng.formulas.FormulaFactory;
+import com.booleworks.logicng.formulas.Literal;
+import com.booleworks.logicng.formulas.Variable;
+import com.booleworks.logicng.solvers.SATSolver;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+
+public class CspModelEnumerationTest extends ParameterizedCspTest {
+
+    @ParameterizedTest
+    @MethodSource("cspFactories")
+    public void testOnlyIntVariables(final CspFactory cf) {
+        final FormulaFactory f = cf.formulaFactory();
+        final IntegerVariable a = cf.variable("a", 1, 2);
+        final IntegerVariable b = cf.variable("b", 10, 12);
+        final IntegerVariable c = cf.variable("c", -5, 12);
+        final Formula formula = cf.eq(cf.add(a, c), b);
+        final Csp csp = cf.buildCsp(formula);
+        final CspEncodingContext context = new CspEncodingContext();
+        final SATSolver solver = SATSolver.newSolver(f);
+        solver.add(cf.encodeCsp(csp, context));
+        final List<CspAssignment> models = CspModelEnumeration.enumerate(solver, csp, context, cf);
+        assertThat(models).hasSize(6);
+        assertThat(models).containsExactlyInAnyOrder(
+                assignmentFrom(a, 2, c, 8, b, 10),
+                assignmentFrom(a, 1, c, 9, b, 10),
+                assignmentFrom(a, 2, c, 9, b, 11),
+                assignmentFrom(a, 1, c, 10, b, 11),
+                assignmentFrom(a, 2, c, 10, b, 12),
+                assignmentFrom(a, 1, c, 11, b, 12)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("cspFactories")
+    public void testOnlyBooleanVariables(final CspFactory cf) {
+        final FormulaFactory f = cf.formulaFactory();
+        final Variable a = f.variable("A");
+        final Variable b = f.variable("B");
+        final Variable c = f.variable("C");
+        final Formula formula = f.or(a, f.and(b, c));
+        final Csp csp = cf.buildCsp(formula);
+        final CspEncodingContext context = new CspEncodingContext();
+        final SATSolver solver = SATSolver.newSolver(f);
+        solver.add(cf.encodeCsp(csp, context));
+        final List<CspAssignment> models = CspModelEnumeration.enumerate(solver, csp, context, cf);
+        assertThat(models).hasSize(5);
+        assertThat(models).containsExactlyInAnyOrder(
+                assignmentFrom(a, b, c),
+                assignmentFrom(a, b.negate(f), c.negate(f)),
+                assignmentFrom(a, b, c.negate(f)),
+                assignmentFrom(a, b.negate(f), c),
+                assignmentFrom(a.negate(f), b, c)
+        );
+    }
+
+    private CspAssignment assignmentFrom(final IntegerVariable v1, final int value1) {
+        final CspAssignment assignment = new CspAssignment();
+        assignment.addIntAssignment(v1, value1);
+        return assignment;
+    }
+
+    private CspAssignment assignmentFrom(final IntegerVariable v1, final int value1, final IntegerVariable v2, final int value2) {
+        final CspAssignment assignment = new CspAssignment();
+        assignment.addIntAssignment(v1, value1);
+        assignment.addIntAssignment(v2, value2);
+        return assignment;
+    }
+
+    private CspAssignment assignmentFrom(final IntegerVariable v1, final int value1, final IntegerVariable v2, final int value2, final IntegerVariable v3, final int value3) {
+        final CspAssignment assignment = new CspAssignment();
+        assignment.addIntAssignment(v1, value1);
+        assignment.addIntAssignment(v2, value2);
+        assignment.addIntAssignment(v3, value3);
+        return assignment;
+    }
+
+    private CspAssignment assignmentFrom(final Literal l1) {
+        final CspAssignment assignment = new CspAssignment();
+        assignment.addLiteral(l1);
+        return assignment;
+    }
+
+    private CspAssignment assignmentFrom(final Literal l1, final Literal l2) {
+        final CspAssignment assignment = new CspAssignment();
+        assignment.addLiteral(l1);
+        assignment.addLiteral(l2);
+        return assignment;
+    }
+
+    private CspAssignment assignmentFrom(final Literal l1, final Literal l2, final Literal l3) {
+        final CspAssignment assignment = new CspAssignment();
+        assignment.addLiteral(l1);
+        assignment.addLiteral(l2);
+        assignment.addLiteral(l3);
+        return assignment;
+    }
+}
